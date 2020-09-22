9e97d091baf3affa2af09d412ccb662b21ef7a82
Camille Meulien <cmeulien@heliostech.fr>
2020-09-22 17:13:52 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_083000) do
     t.string "currency_id", limit: 10, null: false
     t.string "name", limit: 64, null: false
     t.string "description", default: ""
-    t.json "data"
+    t.text "data", limit: 4294967295, collation: "utf8mb4_bin"
     t.integer "pin", limit: 3, null: false, unsigned: true
     t.datetime "sent_at"
     t.integer "state", limit: 1, default: 0, null: false, unsigned: true
@@ -99,7 +99,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_083000) do
     t.decimal "withdraw_limit_24h", precision: 32, scale: 16, default: "0.0", null: false
     t.decimal "withdraw_limit_72h", precision: 32, scale: 16, default: "0.0", null: false
     t.integer "position", null: false
-    t.json "options"
+    t.text "options", limit: 4294967295, collation: "utf8mb4_bin"
     t.boolean "visible", default: true, null: false
     t.boolean "deposit_enabled", default: true, null: false
     t.boolean "withdrawal_enabled", default: true, null: false
@@ -176,7 +176,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_083000) do
     t.string "name", null: false
     t.integer "pointer", unsigned: true
     t.integer "counter"
-    t.json "data"
+    t.text "data", limit: 4294967295, collation: "utf8mb4_bin"
     t.integer "error_code", limit: 1, default: 255, null: false, unsigned: true
     t.string "error_message"
     t.datetime "started_at"
@@ -208,7 +208,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_083000) do
     t.decimal "max_price", precision: 32, scale: 16, default: "0.0", null: false
     t.decimal "min_amount", precision: 32, scale: 16, default: "0.0", null: false
     t.integer "position", null: false
-    t.json "data"
+    t.text "data", limit: 4294967295, collation: "utf8mb4_bin"
     t.string "state", limit: 32, default: "enabled", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -326,8 +326,8 @@ ActiveRecord::Schema.define(version: 2020_09_09_083000) do
     t.decimal "total_debit_value", precision: 48, scale: 16, default: "0.0"
     t.decimal "total_balance_value", precision: 48, scale: 16, default: "0.0"
     t.decimal "average_balance_price", precision: 48, scale: 16, default: "0.0"
-    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "created_at", default: -> { "current_timestamp()" }, null: false
+    t.datetime "updated_at", default: -> { "current_timestamp()" }, null: false
     t.index ["pnl_currency_id", "currency_id", "member_id"], name: "index_currency_ids_and_member_id", unique: true
   end
 
@@ -336,8 +336,8 @@ ActiveRecord::Schema.define(version: 2020_09_09_083000) do
     t.string "currency_id", limit: 10, null: false
     t.string "reference_type", null: false
     t.bigint "last_id"
-    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "created_at", default: -> { "current_timestamp()" }, null: false
+    t.datetime "updated_at", default: -> { "current_timestamp()" }, null: false
     t.index ["pnl_currency_id", "currency_id", "last_id"], name: "index_currency_ids_and_last_id"
     t.index ["pnl_currency_id", "currency_id", "reference_type"], name: "index_currency_ids_and_type", unique: true
   end
@@ -401,7 +401,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_083000) do
     t.integer "kind", null: false
     t.string "gateway", limit: 20, default: "", null: false
     t.string "settings_encrypted", limit: 1024
-    t.json "balance"
+    t.text "balance", limit: 4294967295, collation: "utf8mb4_bin"
     t.decimal "max_balance", precision: 32, scale: 16, default: "0.0", null: false
     t.string "status", limit: 32
     t.datetime "created_at", null: false
@@ -438,7 +438,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_083000) do
     t.string "tid", limit: 64, null: false, collation: "utf8_bin"
     t.string "rid", limit: 256, null: false
     t.string "note", limit: 256
-    t.json "error"
+    t.text "error", limit: 4294967295, collation: "utf8mb4_bin"
     t.datetime "created_at", precision: 3, null: false
     t.datetime "updated_at", precision: 3, null: false
     t.datetime "completed_at", precision: 3
